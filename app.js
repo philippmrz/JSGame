e39d4522b2ef6b9bbffe18c6fc9bc1c2d05dc6a8
Lukas Gremminger <luklg@live.de>
2018-09-19 17:57:34 +0200

    added movement and jump

--- app.js
+++ app.js
@@ -1,102 +1,75 @@
-var canvas = document.querySelector('canvas');
-var context = canvas.getContext('2d');
-var x = canvas.width / 2;
-var y = canvas.height / 2;
-var speedX = 2;
-var speedY = 2;
-var rectWidth = 100;
-var rectHeight = 150;
-const PLAYERCOLOR = '#' + Math.floor(Math.random() * 16777215).toString(16);
-(function (){
+(function(canvas, context) {
+  class Player {
+    constructor() {
+      this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+      this.width = 100;
+      this.height = 150;
+      this.x = 0.1 * canvas.width;
+      this.y = 0.9 * canvas.height - this.height;
+      this.speedX = 10;
+      this.speedY = 10;
+    }
+    update() {
+      if (player.y < 0.9 * canvas.height - player.height && keys[38] == false) {
+        player.y += player.speedY;
+      }
+
+      if (keys[38]) {
+        player.y -= player.speedY;
+      }
+      if (keys[39]) {
+        player.x += player.speedX;
+      }
+      if (keys[37]) {
+        player.x -= player.speedX;
+      }
+    }
+  }
+
+  var player;
+  var keys = [];
   setup();
   requestAnimationFrame(draw);
 
+  function setup() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    context.lineWidth = 6;
+    player = new Player();
+    addEventListener("keydown", function (evt) {
+      keys[evt.keyCode] = true;
+    });
+    document.body.addEventListener("keyup", function (e) {
+      keys[e.keyCode] = false;
+    });
+  }
 
-function setup() {
-  document.addEventListener('keydown', handleKeysPressed);
-  document.addEventListener('keyup', handleKeysUnpressed);
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
-  context.lineWidth = 6;
-  canvas.addEventListener('click',function(){
+  function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-  });
-}
-  switch (evt.key) {
-    case 'ArrowLeft':
-      speedX = 1;
-      speedY = 1;
-      break;
-
-    case 'ArrowRight':
-      speedX = 1;
-      speedY = 1;
-      break;
-
-    case 'ArrowUp':
-      speedX = 1;
-      speedY = 1;
-      break;
-
-    case 'ArrowDown':
-      speedX = 1;
-      speedY = 1;
-      break;
+    player.update();
+    drawBackground();
+    drawPlayer();
+    requestAnimationFrame(draw);
   }
-}
-function handleKeysPressed(evt) {
-  if(x >= canvas.width - rectWidth){
-    x = canvas.width - rectWidth;
-    speedX = - 0.7 * speedX;
+
+  function drawBackground() {
+    context.beginPath();
+    context.fillStyle = '#000000';
+    context.rect(0, 0.9 * canvas.height, canvas.width, 0.1 * canvas.height);
+    context.fill();
+    context.closePath();
   }
-  if(x <= 0) {
-    x = 0;
-    speedX = -0.7 * speedX;
+
+  function drawPlayer (){
+    context.beginPath();
+    context.fillstyle = '#000000';
+    context.rect(player.x, player.y, player.width, player.height);
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.fillStyle = player.color;
+    context.rect(player.x, player.y, player.width, player.height);
+    context.fill();
+    context.closePath();
   }
-  if(y >= canvas.height - rectHeight) {
-    y = canvas.height - rectHeight;
-    speedY = - 0.7 * speedY;
-  }
-  if (y <= 0) {
-    y = 0;
-    speedY = - 0.7 * speedY;
-  }
-switch (evt.key) {
-  case 'ArrowLeft':
-    x = x - speedX;
-    speedX++;
-    break;
-
-  case 'ArrowRight':
-    x = x + speedX;
-    speedX++;
-    console.log('speedX:', speedX);
-    break;
-
-  case 'ArrowUp':
-    y = y - speedY;
-    speedY++;
-    break;
-
-  case 'ArrowDown':
-    y = y + speedY;
-    speedY++;
-    break;
-}
-}
-
-function draw(){
-
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.beginPath()
-  context.fillStyle = '#000000';
-  context.rect(x, y, rectWidth, rectHeight);
-  context.stroke()
-  context.beginPath()
-  context.fillStyle = PLAYERCOLOR;
-  context.rect(x, y, rectWidth, rectHeight);
-  context.fill();
-  requestAnimationFrame(draw);
-}
-
-}());
+}(document.querySelector('canvas'),document.querySelector('canvas').getContext('2d')));
