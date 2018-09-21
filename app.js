df225eaa51a0ccbcd1097afea44c2d7a4f1ea850
Lukas Gremminger <luklg@live.de>
2018-09-21 20:20:03 +0200

    -changed char size -added realistic jumping and running

--- app.js
+++ app.js
@@ -2,30 +2,51 @@
   class Player {
     constructor() {
       this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-      this.width = 100;
-      this.height = 150;
+      this.width = 60;
+      this.height = 90;
       this.x = 0.1 * canvas.width;
       this.y = 0.9 * canvas.height - this.height;
-      this.speedX = 10;
-      this.speedY = 10;
-    }
-    update() {
-      if (player.y < 0.9 * canvas.height - player.height && keys[38] == false) {
-        player.y += player.speedY;
-      }
-
-      if (keys[38] && player.y > 0) {
-        player.y -= player.speedY;
-      }
-      if (keys[39]) {
-        player.x += player.speedX;
-      }
-      if (keys[37]) {
-        player.x -= player.speedX;
-      }
+      this.speedX = 0;
+      this.speedY = 0;
+      this.left = false;
+      this.right = false;
+      this.up = false;
+      this.jumping = false;
     }
   }
+  function update (){
+    if (player.up == true && player.jumping == false) {
 
+      player.speedY -= 50;
+      player.jumping = true;
+
+    }
+
+    if (player.left == true) {
+
+      player.speedX -= 3;
+
+    }
+
+    if (player.right == true) {
+
+      player.speedX += 3;
+
+    }
+    player.speedY += 2.5;
+    player.x += player.speedX;
+    player.y += player.speedY;
+    player.speedX *= 0.9;
+    player.speedX *= 0.9;
+
+    if (player.y > 0.9 * canvas.height - player.height) {
+
+      player.jumping = false;
+      player.y = 0.9 * canvas.height - player.height;
+      player.speedY = 0;
+
+}
+}
   var player;
   var keys = [];
   var img = new Image();
@@ -36,20 +57,32 @@
   function setup() {
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
-    context.lineWidth = 6;
     context.imageSmoothingEnabled = false;
     player = new Player();
-    addEventListener("keydown", function (evt) {
-      keys[evt.keyCode] = true;
-    });
-    document.body.addEventListener("keyup", function (e) {
-      keys[e.keyCode] = false;
-    });
+    addEventListener("keydown", keyListener);
+    document.body.addEventListener("keyup", keyListener);
   }
 
+  function keyListener(evt){
+
+      var key_state = (evt.type == "keydown")?true:false;
+
+        switch(evt.keyCode) {
+
+          case 37:
+            player.left = key_state;
+            break;
+          case 38:
+            player.up = key_state;
+            break;
+          case 39:
+            player.right = key_state;
+            break;
+      }
+  }
   function draw() {
+    update();
     context.clearRect(0, 0, canvas.width, canvas.height);
-    player.update();
     drawBackground();
     drawPlayer();
     requestAnimationFrame(draw);
