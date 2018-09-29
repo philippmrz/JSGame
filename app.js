7e40a06d0ea7e6750c42dc1438840369dc61455a
Philipp Merz <phmrch@gmail.com>
2018-09-29 16:49:55 +0200

    Refractored everything

--- app.js
+++ app.js
@@ -12,18 +12,19 @@
       this.isRight = false;
       this.isUp = false;
       this.isJumping = false;
+      this.currentImage = standingImages[0];
     }
 
     update() {
       if (this.isUp && !this.isJumping) {
         this.speedY -= 30;
         this.isJumping = true;
-       }
+      }
 
-       if (this.isJumping){
-         if (this.isLeft) this.speedX -= 1.25;
-         if (this.isRight) this.speedX += 1.25;
-       }
+      if (this.isJumping) {
+        if (this.isLeft) this.speedX -= 1.25;
+        if (this.isRight) this.speedX += 1.25;
+      }
 
       if (this.isLeft) {
         this.speedX -= 1.5;
@@ -36,8 +37,7 @@
       this.speedY += 2;
       this.x += this.speedX;
       this.y += this.speedY;
-      this.speedX *= 0.9;
-      this.speedX *= 0.9;
+      this.speedX *= 0.87;
 
       if (this.y > 0.9 * canvas.height - this.height) {
         this.isJumping = false;
@@ -45,23 +45,72 @@
         this.speedY = 0;
       }
     }
+
+    changeCurrentImage() {
+      if (this.isRight) {
+        //Add one to index to change currentImage
+        let index = walkRightImages.indexOf(this.currentImage) + 1;
+        if (index == walkRightImages.length) index = 0;
+        this.currentImage = walkRightImages[index];
+      } else if (this.isLeft) {
+        let index = walkLeftImages.indexOf(this.currentImage) + 1;
+        if (index == walkLeftImages.length) index = 0;
+        this.currentImage = walkLeftImages[index];
+      } else {
+        if (walkRightImages.includes(this.currentImage)) {
+          this.currentImage = standingImages[0];
+        }
+        if (walkLeftImages.includes(this.currentImage)) {
+          this.currentImage = standingImages[1];
+        }
+      }
+    }
+
+    draw() {
+      context.beginPath();
+      context.drawImage(this.currentImage, this.x, this.y, this.width, this.height);
+      context.closePath();
+      ticks++;
+    }
+  }
+
+  function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    player.update();
+    if (ticks % 9 == 0) player.changeCurrentImage();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground();
+    player.draw();
   }
 
   function setup() {
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
     context.imageSmoothingEnabled = false;
+
     player = new Player();
-    ticks = 0;
-    img.src = 'animation1-right.png';
-    img1.src = 'animation2-right.png';
-    img2.src = 'animation1-left.png';
-    img3.src = 'animation2-left.png';
-    img4.src = 'standing-right.png';
-    img5.src = 'standing-left.png';
-    currentCharacterImage = img;
+
+    walkRightImages[0].src = 'animation1-right.png';
+    walkRightImages[1].src = 'animation2-right.png';
+    walkLeftImages[0].src = 'animation1-left.png';
+    walkLeftImages[1].src = 'animation2-left.png';
+    standingImages[0].src = 'standing-right.png';
+    standingImages[1].src = 'standing-left.png';
+
     addEventListener("keydown", keyListener);
     addEventListener("keyup", keyListener);
+
+    ticks = 0;
+
+    requestAnimationFrame(gameLoop);
+  }
+
+  function drawBackground() {
+    context.beginPath();
+    context.fillStyle = '#000000';
+    context.rect(0, 0.9 * canvas.height, canvas.width, 0.1 * canvas.height);
+    context.fill();
+    context.closePath();
   }
 
   function keyListener(evt) {
@@ -80,58 +129,12 @@
     }
   }
 
-  function draw() {
-    player.update();
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawBackground();
-    drawPlayer();
-    requestAnimationFrame(draw);
-  }
-
-  function drawBackground() {
-    context.beginPath();
-    context.fillStyle = '#000000';
-    context.rect(0, 0.9 * canvas.height, canvas.width, 0.1 * canvas.height);
-    context.fill();
-    context.closePath();
-  }
-
-  function drawPlayer() {
-    context.beginPath();
-    animatePlayer();
-    context.drawImage(currentCharacterImage, player.x, player.y, player.width, player.height);
-    ticks++;
-    context.closePath();
-  }
-  function animatePlayer() {
-    if (ticks % 9 == 0) {
-      if (player.isRight) {
-        if (currentCharacterImage == img) currentCharacterImage = img1;
-        else currentCharacterImage = img;
-      }
-      else if (player.isLeft) {
-        if (currentCharacterImage == img2) currentCharacterImage = img3;
-        else currentCharacterImage = img2;
-      }
-      else if (!player.isLeft && !player.isRight) {
-        if (currentCharacterImage == img || currentCharacterImage == img1) {
-          currentCharacterImage = img4;
-        }
-        if (currentCharacterImage == img2 || currentCharacterImage == img3) {
-          currentCharacterImage = img5;
-        }
-      }
-    }
-  }
   var player;
   var ticks;
-  var img = new Image();
-  var img1 = new Image();
-  var img2 = new Image();
-  var img3 = new Image();
-  var img4 = new Image();
-  var img5 = new Image();
-  var currentCharacterImage;
+  var walkRightImages = [new Image(), new Image()];
+  var walkLeftImages = [new Image(), new Image()];
+  var standingImages = [new Image(), new Image()];
+
+  //Entrypoint
   setup();
-  requestAnimationFrame(draw);
 }(document.querySelector('canvas'), document.querySelector('canvas').getContext('2d')));
