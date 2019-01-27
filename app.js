38308d9126559817d6006264c8bc034a25fd116f
Lukas Gremminger <luklg@live.de>
2019-01-27 20:43:53 +0100

    removed meme-concrete and fixed a bug (player had to jump to enter the canvas from offstage)

--- app.js
+++ app.js
@@ -178,10 +178,10 @@
     leancup = new Leancup();
     leancupCounter = new LeancupCounter();
     solids = [
-      new Solid(220, 600, 200, 60),
-      new Solid(500, canvas.height - 350, 200, 60),
-      new Solid(800, canvas.height - 500, 200, 60),
-	  new Solid(0, 0.9 * canvas.height, canvas.width, 0.1 * canvas.height)
+      new Solid(220, 500, 200, 60),
+      new Solid(500, 400, 200, 60),
+      new Solid(800, 300, 200, 60),
+      new Solid(0, 0.90001 * canvas.height, canvas.width, 0.1 * canvas.height)
     ];
 
     walkRightImages[0].src = 'assets/animation1-right.png';
